e0748d3b294e9f6da96353b3aaa3bd26349e24ae
Gustavo Henrique <btkgustavo@gmail.com>
2019-04-29 15:29:22 -0300

    Atualizando

--- bytebank-herdado/src/TesteReferencias.java
+++ bytebank-herdado/src/TesteReferencias.java
@@ -1,19 +1,29 @@
 
 public class TesteReferencias {
+
 	public static void main(String[] args) {
 		
-		Funcionarios g1 = new Gerentes();
+		Gerente g1 = new Gerente();
 		g1.setNome("Marcos");
-		g1.setSalario(5000);
+		g1.setSalario(5000.0);
+
+		Funcionario f = new Funcionario();
+		f.setSalario(2000.0);
+	
+		EditorVideo ev = new EditorVideo();
+		ev.setSalario(2500.0);
 		
-		Funcionarios e = new EditoresdeVideo();
-		e.setSalario(2500);
-		Funcionarios f = new Funcionarios();
-		f.setSalario(2000);
+		Designer d = new Designer();
+		d.setSalario(2000.0);
+		
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(g1);
 		controle.registra(f);
-		controle.registra(e);
+		controle.registra(ev);
+		controle.registra(d);
+		
 		System.out.println(controle.getSoma());
+		
 	}
+
 }
